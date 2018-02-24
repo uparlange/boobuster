cf0dd7b8aca4d3c4649c2562dab2faa60fdb0540
Ugo Parlange <uparlange@gmail.com>
2018-02-24 23:38:13 +0100

    Correct port for Heroku

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 app.use(express.static("www"));
 app.use("/vendors", express.static("vendors"));
 app.use("/vendors", express.static("node_modules/pixi.js/dist"));
-app.listen(3000, function () {
-    console.log("Listening on port 3000!");
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("Listening on port " + port + "!");
 })
